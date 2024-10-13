fc92cc0db2fba3d00cacae66bb9d47c95de326c2
jrupinski <rupinskijakub@gmail.com>
2024-10-13 16:54:28 +0200

    feat: Add first version of my config

---
+++ plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true }
+    })
+  end
+}
