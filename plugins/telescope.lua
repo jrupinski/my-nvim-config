5795521a72a2a709a78e94c50398a3e45c22a2cd
jrupinski <rupinskijakub@gmail.com>
2024-10-15 13:47:20 +0200

    feat: Enable fuzzy search in telescope

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -23,4 +23,12 @@ return {
 			require("telescope").load_extension("ui-select")
 		end,
 	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+		config = function()
+			require("telescope").setup()
+			require("telescope").load_extension("fzf")
+		end,
+	},
 }
