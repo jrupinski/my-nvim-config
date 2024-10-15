2e532be0bac44bea4b28973c76e46629c69cc9cb
jrupinski <rupinskijakub@gmail.com>
2024-10-15 14:05:40 +0200

    feat: Add <leader>gg keybind Fugitive toggle

--- plugins/fugitive.lua
+++ plugins/fugitive.lua
@@ -1,3 +1,16 @@
 return {
-	"tpope/vim-fugitive",
+  "tpope/vim-fugitive",
+  config = function()
+    vim.keymap.set("n", "<leader>gg", function()
+      local windows = vim.api.nvim_list_wins()
+      for _, v in pairs(windows) do
+        local status, _ = pcall(vim.api.nvim_win_get_var, v, "fugitive_status")
+        if status then
+          vim.api.nvim_win_close(v, false)
+          return
+        end
+      end
+      vim.cmd([[Git]])
+    end, { desc = "[G]it to[G]gle" })
+  end,
 }
