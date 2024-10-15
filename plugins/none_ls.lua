fd83e55ce03ebac2e3be526eeb2ec7ed1ebb70a6
jrupinski <rupinskijakub@gmail.com>
2024-10-15 11:16:18 +0200

    feat: Add a couple more lint and formatters

--- plugins/none_ls.lua
+++ plugins/none_ls.lua
@@ -4,9 +4,13 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.diagnostics.yamllint,
+				null_ls.builtins.formatting.erb_lint,
+				null_ls.builtins.formatting.htmlbeautifier,
+				null_ls.builtins.formatting.mdformat,
+				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.formatting.stylua,
 			},
 		})
 
