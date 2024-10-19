b2e8f0715a447b49dd1754ce97a02e5b43d129ca
jrupinski <rupinskijakub@gmail.com>
2024-10-19 15:42:50 +0200

    feat: Add fuzzy live grep to telescope

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -7,13 +7,13 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			-- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
 			-- vim.keymap.set("n", "<leader>fr", builtin.resume, {})
 			-- vim.keymap.set("n", "<leader>fs", builtin.builtin, {})
 			-- vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-			-- vim.keymap.set("n", "<leader>f.", builtin.oldfiles, {})
-			-- vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>f.", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
 		end,
 	},
 	{
@@ -36,6 +36,17 @@ return {
 		config = function()
 			require("telescope").setup()
 			require("telescope").load_extension("fzf")
+		end,
+	},
+	-- Fuzzy find in live_grep
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("fzf-lua").setup({})
+			local builtin = require("fzf-lua")
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep_native, {})
 		end,
 	},
 }
